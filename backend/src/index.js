cc66be18f9156b0d85f451e6575afa60bf984c43
karthikajay04 <karthikajay2004@gmail.com>
2025-12-07 00:18:25 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // ----------------- Middleware -----------------
 app.use(cors({
-  origin: "*", // or "http://localhost:5173"
+  origin: "https://frontend-k5qm.onrender.com", // or "http://localhost:5173"
   credentials: true
 }));
 
